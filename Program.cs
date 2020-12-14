7b51d3f21a7c187a2ef23851f3e4e74f2b65bfc5
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-14 10:46:20 +0200

    adding example with LIST

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CommonTutorilas
 {
@@ -7,9 +8,23 @@ namespace CommonTutorilas
         static void Main(string[] args)
         {
 
-            var x = 34.1;  // keywaord var is kind of implicit typing, it decides alone the type of the literal;
-            double y = 10.3;
-           
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };  //array initialisation  //array has a fixed size !
+            // var alone make decision of the type of the variable ;
+            var grades = new List<double> { 12.7, 10.3, 6.11, 4.1 };  // < -this is an angle bracket // Lsit has dynamic size-chaneagable
+            // list is dynamically-sizable collection
+            grades.Add(56.1);
+            // Console.WriteLine(grades[0]); 
+
+
+            var result = 0.0;
+            foreach (double number in grades)
+            {
+                result += number;
+                //Console.WriteLine(result);
+            }
+            Console.WriteLine(result);
+
+
 
             if (args.Length > 0)
             {
@@ -19,7 +34,7 @@ namespace CommonTutorilas
             {
                 Console.WriteLine("Hello!");
             }
-             // Console.WriteLine(args.Length);  // the array [] with a name : args doesn have filled elements
+            // Console.WriteLine(args.Length);  // the array [] with a name : args doesn have filled elements
         }
     }
 }
